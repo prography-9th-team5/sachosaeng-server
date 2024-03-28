babb259950d0573597e1e8983fedcc6f3db0ae03
hectick <ch.yeondev@gmail.com>
2024-03-28 14:35:03 +0900

    feat: 스웨거 설정 추가

---
+++ server/src/main/java/prography/team5/server/config/OpenApiConfig.java
@@ -0,0 +1,11 @@
+package prography.team5.server.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Swagger API Docs", description = "API 명세서"))
+public class OpenApiConfig {
+
+}
