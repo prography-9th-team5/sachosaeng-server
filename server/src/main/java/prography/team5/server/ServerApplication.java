6112d6a16bb4bdc4d8bea8ac1fb3b9312befe12b
hectick <ch.yeondev@gmail.com>
2024-03-28 16:04:35 +0900

    feat: 타임존 설정

--- server/src/main/java/prography/team5/server/ServerApplication.java
+++ server/src/main/java/prography/team5/server/ServerApplication.java
@@ -1,10 +1,17 @@
 package prography.team5.server;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
