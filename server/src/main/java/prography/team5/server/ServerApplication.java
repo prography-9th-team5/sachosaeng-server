8ca0ace3fb75a6bae5d22d9dc385180bde108e47
hectick <ch.yeondev@gmail.com>
2024-04-28 01:17:17 +0900

    feat: 생성 시각, 수정 시각 기록하는 기본 엔티티 생성

--- server/src/main/java/prography/team5/server/ServerApplication.java
+++ server/src/main/java/prography/team5/server/ServerApplication.java
@@ -4,7 +4,9 @@ import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 
