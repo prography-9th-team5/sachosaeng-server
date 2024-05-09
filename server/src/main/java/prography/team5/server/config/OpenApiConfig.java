5c7284f0c1bdfcd5793914f85ac7faac29e79aa8
hectick <ch.yeondev@gmail.com>
2024-05-10 00:43:14 +0900

    fix: 스웨거 CORS 에러 해결

--- server/src/main/java/prography/team5/server/config/OpenApiConfig.java
+++ server/src/main/java/prography/team5/server/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,6 +26,7 @@ public class OpenApiConfig {
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
 
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .components(new Components().addSecuritySchemes(jwtSchemeName, securityScheme))
                 .addSecurityItem(securityRequirement);
     }
