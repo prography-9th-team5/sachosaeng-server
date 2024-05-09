17db5479cde99556384036580bf1e765f23bd914
hectick <ch.yeondev@gmail.com>
2024-05-09 21:40:40 +0900

    feat: RefreshToken 엔티티 생성

---
+++ server/src/main/java/prography/team5/server/domain/RefreshToken.java
@@ -0,0 +1,35 @@
+package prography.team5.server.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "member_id")
+    private Long userId;
+    private String token;
+    private LocalDateTime expiredAt;
+
+    public RefreshToken(final Long userId, final String token, final LocalDateTime expiredAt) {
+        this.userId = userId;
+        this.token = token;
+        this.expiredAt = expiredAt;
+    }
+
+    public boolean isExpired() {
+        return expiredAt.isBefore(LocalDateTime.now());
+    }
+}
---
+++ server/src/main/java/prography/team5/server/domain/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package prography.team5.server.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+}
