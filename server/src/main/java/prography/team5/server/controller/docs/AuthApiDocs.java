c58414eef47423882b04336d893b32e0844bfcc9
hectick <ch.yeondev@gmail.com>
2024-05-10 01:14:07 +0900

    docs: 문서 업데이트

--- server/src/main/java/prography/team5/server/controller/docs/AuthApiDocs.java
+++ server/src/main/java/prography/team5/server/controller/docs/AuthApiDocs.java
@@ -30,8 +30,8 @@ public interface AuthApiDocs {
     ResponseEntity<CommonApiResponse<LoginResponse>> login(final EmailRequest emailRequest);
 
     @Operation(
-            summary = "엑세스 토큰 재발급 API",
-            description = "리프레시 토큰으로 엑세스 토큰을 재발급 할 수 있습니다."
+            summary = "엑세스 토큰 재발급 API -> 스웨거에서 작동 안됨ㅜ",
+            description = "Cookie에 Refresh={리프레시 토큰}을 담아 보내면 엑세스 토큰을 재발급 할 수 있습니다."
     )
     @ApiResponse(responseCode = "200", description = "엑세스 토큰 재발급 성공입니다.")
     ResponseEntity<CommonApiResponse<AccessTokenResponse>> refreshAccessToken(
@@ -40,7 +40,7 @@ public interface AuthApiDocs {
 
     @Operation(
             summary = "인증 테스트 API",
-            description = "헤더에 Bearer {엑세스 토큰}을 담아 보냈을 때 응답이 성공하는지 테스트 할 수 있습니다."
+            description = "Authorization 헤더에 Bearer {엑세스 토큰}을 담아 보냈을 때 응답이 성공하는지 테스트 할 수 있습니다."
     )
     @ApiResponse(responseCode = "200", description = "인증 성공입니다.")
     ResponseEntity<CommonApiResponse<Void>> test(@Parameter(hidden = true) final VerifiedUser verifiedUser);
