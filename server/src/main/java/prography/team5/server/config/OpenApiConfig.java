2058b58de236bf526dbdd709d79a6881678dc31a
hectick <ch.yeondev@gmail.com>
2024-05-10 00:01:15 +0900

    docs: 인증 관련 api 성공 케이스 문서화

--- server/src/main/java/prography/team5/server/config/OpenApiConfig.java
+++ server/src/main/java/prography/team5/server/config/OpenApiConfig.java
@@ -2,10 +2,30 @@ package prography.team5.server.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "사초생 API Docs", description = "사초생 API 명세서입니다."))
 public class OpenApiConfig {
 
+    @Bean
+    public OpenAPI openAPI(){
+        String jwtSchemeName = "JWT Auth";
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes(jwtSchemeName, securityScheme))
+                .addSecurityItem(securityRequirement);
+    }
 }
