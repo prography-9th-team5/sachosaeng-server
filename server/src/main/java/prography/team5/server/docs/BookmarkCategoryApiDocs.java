598660f2522f07b94b04fc1fc6b25f4bdcdc8e27
hectick <ch.yeondev@gmail.com>
2024-05-26 17:05:14 +0900

    docs: 스웨거 수정

--- server/src/main/java/prography/team5/server/docs/BookmarkCategoryApiDocs.java
+++ server/src/main/java/prography/team5/server/docs/BookmarkCategoryApiDocs.java
@@ -12,7 +12,7 @@ import prography.team5.server.service.auth.dto.Accessor;
 import prography.team5.server.service.dto.BookmarkCategoryRequest;
 import prography.team5.server.service.dto.CategoryResponse;
 
-@Tag(name = "4. 관심 카테고리", description = "관심 카테고리 관련 기능입니다..")
+@Tag(name = "4. 관심 카테고리", description = "관심 카테고리 관련 기능입니다.")
 public interface BookmarkCategoryApiDocs {
 
     @Operation(
@@ -25,7 +25,7 @@ public interface BookmarkCategoryApiDocs {
 
     @Operation(
             summary = "관심 카테고리 갱신 API",
-            description = "유저의 관심 카테고리를 갱신할 수 있습니다."
+            description = "유저의 관심 카테고리를 갱신할 수 있습니다. body에 담아 보낸 카테고리들로 관심 카테고리가 전체 갱신 됩니다."
     )
     @ApiResponse(responseCode = "200", description = "관심 카테고리 갱신 성공입니다.")
     ResponseEntity<CommonApiResponse<Void>> updateByUserId(
