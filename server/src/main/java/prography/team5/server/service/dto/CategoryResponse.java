9a6edce274fcd03e2cd1a8edd17af37be4fb0e5c
hectick <ch.yeondev@gmail.com>
2024-06-05 00:32:33 +0900

    refactor: 이름 수정

--- server/src/main/java/prography/team5/server/service/dto/CategoryResponse.java
+++ server/src/main/java/prography/team5/server/service/dto/CategoryResponse.java
@@ -3,7 +3,7 @@ package prography.team5.server.service.dto;
 import java.util.List;
 import prography.team5.server.domain.category.Category;
 
-public record CategoryResponse(Long id, String name) {
+public record CategoryResponse(Long categoryId, String name) {
 
     public static List<CategoryResponse> from(final List<Category> categories) {
         return categories.stream()
