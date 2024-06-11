62e932b10bd472cdf4e75b27ea3c401bc84737fc
hectick <ch.yeondev@gmail.com>
2024-06-12 01:17:51 +0900

    feat: 관리자가 카테고리와 투표 토픽을 조회하는 간단한 페이지 구현

--- server/src/main/java/prography/team5/server/admin/controller/AdminController.java
+++ server/src/main/java/prography/team5/server/admin/controller/AdminController.java
@@ -1,18 +1,44 @@
 package prography.team5.server.admin.controller;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import prography.team5.server.service.CategoryService;
+import prography.team5.server.service.VoteService;
+import prography.team5.server.service.dto.CategoryResponse;
+import prography.team5.server.service.dto.VoteResponse;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final VoteService voteService;
+    private final CategoryService categoryService;
+
     @GetMapping
     public ModelAndView home(ModelAndView modelAndView) {
-        modelAndView.addObject("message", "Welcome to Admin Page");
+        // modelAndView.addObject("message", "Welcome to Admin Page");
         modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @GetMapping("/categories")
+    public ModelAndView categories(ModelAndView modelAndView) {
+        final List<CategoryResponse> categories = categoryService.findAll();
+        modelAndView.addObject("categories", categories);
+        modelAndView.setViewName("categories");
+        return modelAndView;
+    }
+
+    @GetMapping("/votes")
+    public ModelAndView votes(ModelAndView modelAndView) {
+        final List<VoteResponse> votes = voteService.findAll(null, null, 20); //todo: 게시글 많을때도 옛날 게시글 조회되게!
+        modelAndView.addObject("votes", votes);
+        modelAndView.setViewName("votes");
         return modelAndView;
     }
 }
