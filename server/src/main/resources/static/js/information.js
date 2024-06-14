d5fbe96e4fbd19eebc7a8e071315c16a803c4080
hectick <ch.yeondev@gmail.com>
2024-06-14 15:24:52 +0900

    feat: 관리자 정보 카드 조회 페이지 수정

---
+++ server/src/main/resources/static/js/information.js
@@ -0,0 +1,65 @@
+function openAddModal() {
+    document.getElementById("modal").style.display = "block";
+}
+
+function closeAddModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('/categories')
+        .then(response => response.json())
+        .then(data => {
+            const categoryCheckboxes = document.getElementById('categoryCheckboxes');
+            data.data.forEach(category => {
+                const categoryDiv = document.createElement('div');
+                categoryDiv.classList.add('category');
+
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.id = `category_${category.categoryId}`;
+                checkbox.name = 'categories';
+                checkbox.value = category.categoryId;
+
+                const label = document.createElement('label');
+                label.htmlFor = `category_${category.categoryId}`;
+                label.textContent = category.name;
+
+                categoryDiv.appendChild(checkbox);
+                categoryDiv.appendChild(label);
+                categoryCheckboxes.appendChild(categoryDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Failed to fetch categories. Please try again later.');
+        });
+});
+
+document.getElementById('informationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const categories = Array.from(formData.getAll('categories'));
+    const requestBody = {
+        title: formData.get('title'),
+        content: formData.get('content'),
+        categoryIds: categories.map(Number)
+    };
+
+    fetch('/information', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message); // Show success message or handle response accordingly
+            location.reload();// Optionally, you can redirect or update the UI
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again later.');
+        });
+});
