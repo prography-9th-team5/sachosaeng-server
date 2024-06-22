783bc46456495bd97ed3d2d6e77466a52d9d7a96
hectick <ch.yeondev@gmail.com>
2024-06-22 22:50:05 +0900

    fix: 원상복구2

--- server/src/main/java/prography/team5/server/card/HotVoteApiDocs.java
+++ server/src/main/java/prography/team5/server/card/HotVoteApiDocs.java
@@ -12,9 +12,9 @@ import prography.team5.server.common.CommonApiResponse;
 public interface HotVoteApiDocs {
 
     @Operation(
-            summary = "인기 투표 3개 조회 API",
+            summary = "[홈화면] 인기 투표 3개 조회 API",
             description = """
-                        [홈화면] 별도의 파라미터가 전달되지 않는다면 전체 카테고리를 통틀어 인기투표 3개를 조회합니다. \n
+                        전체 카테고리를 통틀어 인기투표 3개를 조회합니다. \n
                         현재는 최신순으로 3개 조회하는 중, 인기 투표 노출 로직은 추후 수정 예정입니다.
                         """
     )
