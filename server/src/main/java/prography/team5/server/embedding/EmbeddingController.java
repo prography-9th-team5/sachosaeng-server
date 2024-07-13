d05d6a9637ce949f086c4db1fb24cdc141a07255
hectick <ch.yeondev@gmail.com>
2024-07-13 21:57:18 +0900

    feat: api 숨김

--- server/src/main/java/prography/team5/server/embedding/EmbeddingController.java
+++ server/src/main/java/prography/team5/server/embedding/EmbeddingController.java
@@ -1,5 +1,6 @@
 package prography.team5.server.embedding;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +16,10 @@ public class EmbeddingController {
 
     private final EmbeddingService embeddingService;
 
+    // todo: 주기적으로 api 호출로 임베딩할지? 아니면 정보 콘텐츠가 추가될때마다 알아서 임베딩될지?
+    @Hidden
     @GetMapping("/ai/embedding")
-    public ResponseEntity<CommonApiResponse<SimpleVoteWithCategoryResponse>> embed() {
+    public ResponseEntity<CommonApiResponse<Void>> embed() {
         embeddingService.embed();
         return ResponseEntity.ok()
                 .body(new CommonApiResponse<>(0, "API 요청이 성공했습니다."));
