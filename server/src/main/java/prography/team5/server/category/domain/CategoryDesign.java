b1c770a75a87cbd148e2e7c4c5e30f121fc4ea4b
hectick <ch.yeondev@gmail.com>
2024-07-29 23:08:39 +0900

    feat: 카테고리 이미지 테스트

--- server/src/main/java/prography/team5/server/category/domain/CategoryDesign.java
+++ server/src/main/java/prography/team5/server/category/domain/CategoryDesign.java
@@ -13,6 +13,7 @@ public class CategoryDesign {
     private static final String DEFAULT_BACKGROUND_COLOR = "#D8D8D8";
     private static final String DEFAULT_TEXT_COLOR = "#000000";
     private static final String DEFAULT_PX = "40px";
+    private static final String DEFAULT_X = "1x";
 
     private String iconUrl;
     private String textColor;
@@ -24,12 +25,14 @@ public class CategoryDesign {
 
     public String getIconUrl18px() {
         if(iconUrl == null) return null;
-        return iconUrl.replace(DEFAULT_PX, "18px");
+        //return iconUrl.replace(DEFAULT_PX, "18px");
+        return iconUrl.replace(DEFAULT_PX, "32px").replace(DEFAULT_X, "4x");
     }
 
     public String getIconUrl32px() {
         if(iconUrl == null) return null;
-        return iconUrl.replace(DEFAULT_PX, "32px").replace("1x", "2x");
+        //return iconUrl.replace(DEFAULT_PX, "32px").replace(DEFAULT_X, "2x");
+        return iconUrl.replace(DEFAULT_PX, "32px").replace(DEFAULT_X, "4x");
     }
 
     public String getTextColor() {
