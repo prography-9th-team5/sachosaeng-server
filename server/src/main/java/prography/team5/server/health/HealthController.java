e4e7907a6bc8ab6de572eb32758b1de78faf574a
hectick <ch.yeondev@gmail.com>
2024-08-24 18:34:37 +0900

    feat: 헬스체크 api

--- server/src/main/java/prography/team5/server/health/HealthController.java
+++ server/src/main/java/prography/team5/server/health/HealthController.java
@@ -1,20 +1,17 @@
 package prography.team5.server.health;
 
-import java.util.List;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import prography.team5.server.auth.controller.AuthRequired;
-import prography.team5.server.auth.service.dto.Accessor;
-import prography.team5.server.category.service.dto.CategoryResponse;
 import prography.team5.server.common.CommonApiResponse;
 
 @RestController
 @RequestMapping("/health")
 public class HealthController {
 
+    @Hidden
     @GetMapping
     public ResponseEntity<CommonApiResponse<Void>> checkHealth() {
         return ResponseEntity.ok()
