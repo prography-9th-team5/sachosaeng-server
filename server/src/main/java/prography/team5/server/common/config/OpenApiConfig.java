1c7c3b0425dd585348c4f75981cad66e78cde0ee
hectick <ch.yeondev@gmail.com>
2024-09-22 02:39:02 +0900

    feat: swagger 문서화

--- server/src/main/java/prography/team5/server/common/config/OpenApiConfig.java
+++ server/src/main/java/prography/team5/server/common/config/OpenApiConfig.java
@@ -20,6 +20,10 @@ import org.springframework.context.annotation.Configuration;
                     에러 문서 보러가기-> https://bit.ly/sachosaeng-error-docs \n
                     이제부터 api 추가/수정되는 부분은 아래에 써놓을게요. 궁금한점 이상한점 원하는점은 카톡으로 쏴주세요. \n
                     
+                    [09/22 업데이트]
+                    1. 리프레시 토큰을 이용해 엑세스 토큰을 재발급할 때, 리프레시 토큰도 같이 새로 발급하여 필드에 담아 보내도록 하였습니다. 기존 리프레시 토큰은 폐기됩니다. \n
+                    2. refresh, login api 의 경우 X-device 헤더에 기기정보를 같이 담아 보내면(선택이예요), 응답으로 발급된 리프레시 토큰을 해당 기기에서만 사용할 수 있도록 보안이 강화됩니다. \n
+                    
                     [09/19 업데이트]
                     1. 단일 투표 조회에 복수선택이 가능한지를 나타내는 isMultipleChoiceAllowed 필드가 추가되었습니다. \n
                     2. 유저 정보 조회할때 userType을 영어로 주도록 바꾸었습니다.
