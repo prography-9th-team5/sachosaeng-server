853400622ebb4e0daa0429a2d31ab710747f6d5e
hectick <ch.yeondev@gmail.com>
2024-10-17 20:44:08 +0900

    feat: APPROVED 상태의 투표만 조회되도록 수정

--- server/src/main/java/prography/team5/server/card/repository/VoteCardRepository.java
+++ server/src/main/java/prography/team5/server/card/repository/VoteCardRepository.java
@@ -10,31 +10,35 @@ import prography.team5.server.card.domain.VoteCard;
 
 public interface VoteCardRepository extends JpaRepository<VoteCard, Long> {
 
+    //관리자
     @Query("SELECT c FROM VoteCard c ORDER BY c.id DESC")
     Slice<VoteCard> findLatestCards(PageRequest pageRequest);
 
+    //관리자
     @Query("SELECT c FROM VoteCard c WHERE c.id < :cursor ORDER BY c.id DESC")
     Slice<VoteCard> findBeforeCursor(@Param("cursor") Long cursor, PageRequest pageRequest);
 
+    //todo: 관리자는 모든 상태가 조회되도록
     @Query("SELECT c FROM VoteCard c JOIN c.categories cat " +
-            "WHERE cat.id = :categoryId ORDER BY c.id DESC")
+            "WHERE cat.id = :categoryId AND c.approvalStatus = 'APPROVED' ORDER BY c.id DESC")
     Slice<VoteCard> findLatestCardsByCategoriesId(@Param("categoryId") long categoryId, PageRequest pageRequest);
 
+    //todo: 관리자는 모든 상태가 조회되도록
     @Query("SELECT c FROM VoteCard c JOIN c.categories cat " +
-            "WHERE cat.id = :categoryId AND c.id < :cursor ORDER BY c.id DESC")
+            "WHERE cat.id = :categoryId AND c.id < :cursor AND c.approvalStatus = 'APPROVED' ORDER BY c.id DESC")
     Slice<VoteCard> findByCategoriesIdBeforeCursor(
             @Param("cursor") Long cursor,
             @Param("categoryId") long categoryId,
             PageRequest pageRequest
     );
 
-    @Query("SELECT c FROM VoteCard c ORDER BY c.participantCount ASC, c.id ASC")
+    @Query("SELECT c FROM VoteCard c WHERE c.approvalStatus = 'APPROVED' ORDER BY c.participantCount ASC, c.id ASC")
     List<VoteCard> findWithFewestParticipants(PageRequest pageRequest);
 
-    @Query("SELECT MIN(c.participantCount) FROM VoteCard c")
+    @Query("SELECT MIN(c.participantCount) FROM VoteCard c WHERE c.approvalStatus = 'APPROVED'")
     Integer findMinParticipantCount();
 
-    @Query("SELECT c FROM VoteCard c WHERE c.participantCount = :minCount ORDER BY c.id ASC")
+    @Query("SELECT c FROM VoteCard c WHERE c.participantCount = :minCount AND c.approvalStatus = 'APPROVED' ORDER BY c.id ASC")
     List<VoteCard> findWithFewestParticipants(@Param("minCount") Integer minCount);
 
     @Query("SELECT c FROM VoteCard c " +
